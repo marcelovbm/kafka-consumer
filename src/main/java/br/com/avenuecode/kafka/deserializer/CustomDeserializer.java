52e8e70e154a4b6f01567c3c128d34a5ddc6be52
Marcelo Magrinelli <marcelo.magrinelli@avenuecode.com>
2022-07-06 13:19:46 -0300

    Refactoring

--- src/main/java/br/com/avenuecode/kafka/deserializer/CustomDeserializer.java
+++ src/main/java/br/com/avenuecode/kafka/deserializer/CustomDeserializer.java
@@ -1,8 +1,6 @@
 package br.com.avenuecode.kafka.deserializer;
 
 import br.com.avenuecode.kafka.entity.TruckCoordinates;
-import com.fasterxml.jackson.core.exc.StreamReadException;
-import com.fasterxml.jackson.databind.DatabindException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.apache.kafka.common.serialization.Deserializer;
 
@@ -10,20 +8,16 @@ import java.io.IOException;
 
 public class CustomDeserializer implements Deserializer<TruckCoordinates> {
 
-    private ObjectMapper objectMapper = new ObjectMapper();
+    private final ObjectMapper objectMapper = new ObjectMapper();
 
     @Override
     public TruckCoordinates deserialize(String topic, byte[] bytes) {
-        TruckCoordinates record = null;
+        TruckCoordinates truckCoordinates = null;
         try{
-            record = objectMapper.readValue(bytes, TruckCoordinates.class);
-        } catch (StreamReadException e) {
-            e.printStackTrace();
-        } catch (DatabindException e) {
-            e.printStackTrace();
+            truckCoordinates = objectMapper.readValue(bytes, TruckCoordinates.class);
         } catch (IOException e) {
             e.printStackTrace();
         }
-        return record;
+        return truckCoordinates;
     }
 }
