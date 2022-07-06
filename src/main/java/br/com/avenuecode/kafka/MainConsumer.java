2cbc89528d80e318fec2ea9da19743f9a90f6682
Marcelo Magrinelli <marcelo.magrinelli@avenuecode.com>
2022-07-06 13:49:23 -0300

    Using ConsumerConfig class

--- src/main/java/br/com/avenuecode/kafka/MainConsumer.java
+++ src/main/java/br/com/avenuecode/kafka/MainConsumer.java
@@ -2,6 +2,7 @@ package br.com.avenuecode.kafka;
 
 import br.com.avenuecode.kafka.deserializer.CustomDeserializer;
 import br.com.avenuecode.kafka.entity.TruckCoordinates;
+import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.apache.kafka.clients.consumer.ConsumerRecords;
 import org.apache.kafka.clients.consumer.KafkaConsumer;
@@ -13,20 +14,21 @@ import java.util.Properties;
 
 public class MainConsumer {
 
-    private static final String TOPIC_NAME = "avenuecode-kafka-custom";
+    private static final String TOPIC_NAME = "avenuecode-partitions";
 
     public static void main(String[] args) {
         Properties properties = new Properties();
-        properties.setProperty("bootstrap.servers", "localhost:9092");
-        properties.setProperty("key.deserializer", LongDeserializer.class.getName());
-        properties.setProperty("value.deserializer", CustomDeserializer.class.getName());
-        properties.setProperty("group.id", "avenueCodeGroup");
+        properties.setProperty(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        properties.setProperty(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, LongDeserializer.class.getName());
+        properties.setProperty(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, CustomDeserializer.class.getName());
+        properties.setProperty(ConsumerConfig.GROUP_ID_CONFIG, "avenueCodeGroup");
 
         try(KafkaConsumer<Long, TruckCoordinates> consumer = new KafkaConsumer<>(properties)){
             consumer.subscribe(Collections.singletonList(TOPIC_NAME));
             ConsumerRecords<Long, TruckCoordinates> consumerRecords = consumer.poll(Duration.ofSeconds(20));
             for (ConsumerRecord<Long, TruckCoordinates> consumerRecord: consumerRecords) {
                 System.out.println("Key: " + consumerRecord.key() + " Value: " + consumerRecord.value());
+                System.out.println("Partition: " + consumerRecord.partition());
             }
         } catch (Exception e){
             e.printStackTrace();
